c0fdd4ee831279eff0fcab60a20a580722bcc316
santiracca <santiagoracca7@gmail.com>
2021-01-13 19:36:12 -0300

    add list creation

--- src/components/CreateList.jsx
+++ src/components/CreateList.jsx
@@ -1,40 +1,91 @@
 import React from "react";
-// import * as db from "../firestore";
+import * as db from "../firestore";
+import { mutate } from "swr";
 
-function CreateList() {
+const DEFAULT_LIST = {
+  name: "",
+  description: "",
+  image: null,
+};
+
+function CreateList({ user }) {
+  const [list, setList] = React.useState(DEFAULT_LIST);
+  const [submitting, setSubmitting] = React.useState(false);
+
+  const handleInputChange = (event) => {
+    const { name, value, files } = event.target;
+    if (files) {
+      const image = files[0];
+      console.log(image);
+      setList((prev) => ({
+        ...prev,
+        image,
+      }));
+    } else {
+      setList((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    }
+  };
+
+  const handleCreateList = async () => {
+    try {
+      setSubmitting(true);
+      await db.createList(list, user);
+      mutate(user.uid);
+      setList(DEFAULT_LIST);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
   return (
-    <div className="flex flex-col text-center w-full mb-12">
-      <h1 className="text-2xl font-medium title-font mb-4 text-white tracking-widest">
-        WELCOME, USER!
+    <div className='flex flex-col text-center w-full mb-12'>
+      <h1 className='text-2xl font-medium title-font mb-4 text-white tracking-widest'>
+        WELCOME, {user.displayName.toUpperCase()}!
       </h1>
-      <p className="lg:w-2/3 mx-auto mb-12 leading-relaxed text-base">
+      <p className='lg:w-2/3 mx-auto mb-12 leading-relaxed text-base'>
         To get started, create a list with a name and a cover image
       </p>
-      <div className="lg:w-2/6 mx-auto md:w-1/2 bg-gray-800 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+      <div className='lg:w-2/6 mx-auto md:w-1/2 bg-gray-800 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0'>
         <input
-          className="bg-gray-900 rounded border text-white border-gray-900 focus:outline-none focus:border-green-500 text-base px-4 py-2 mb-4"
-          placeholder="Add list name"
-          type="text"
-          name="name"
+          className='bg-gray-900 rounded border text-white border-gray-900 focus:outline-none focus:border-green-500 text-base px-4 py-2 mb-4'
+          placeholder='Add list name'
+          type='text'
+          name='name'
+          onChange={handleInputChange}
+          value={list.name}
           required
         />
         <textarea
-          className="bg-gray-900 rounded border text-white border-gray-900 focus:outline-none focus:border-green-500 text-base px-4 py-2 mb-4"
-          placeholder="Add short description"
-          type="text"
-          name="description"
+          className='bg-gray-900 rounded border text-white border-gray-900 focus:outline-none focus:border-green-500 text-base px-4 py-2 mb-4'
+          placeholder='Add short description'
+          type='text'
+          onChange={handleInputChange}
+          value={list.description}
+          name='description'
         />
         <input
-          className="bg-gray-900 rounded border text-white border-gray-900 focus:outline-none focus:border-green-500 text-base px-4 py-2 mb-4"
-          placeholder="Add list name"
-          type="file"
-          name="image"
+          className='bg-gray-900 rounded border text-white border-gray-900 focus:outline-none focus:border-green-500 text-base px-4 py-2 mb-4'
+          placeholder='Add list name'
+          type='file'
+          onChange={handleInputChange}
+          name='image'
         />
-        {/* display preview image */}
-        <button className="text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">
-          Create List
+        {list.image && (
+          <img className='mb-4' src={URL.createObjectURL(list.image)} />
+        )}
+        <button
+          disabled={!submitting}
+          onClick={handleCreateList}
+          className='text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg'
+        >
+          {submitting ? "Creating..." : " Create List"}
         </button>
-        <p className="text-xs text-gray-600 mt-3">*List name required</p>
+        <p className='text-xs text-gray-600 mt-3'>*List name required</p>
       </div>
     </div>
   );
--- src/firestore/index.js
+++ src/firestore/index.js
@@ -3,9 +3,93 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
 
-// const firebaseApp = !firebase.apps.length
-//   ? firebase.initializeApp(firebaseConfig)
-//   : firebase.app();
-// const db = firebaseApp.firestore();
-// const auth = firebaseApp.auth();
-// const storage = firebaseApp.storage();
+var firebaseConfig = {
+  apiKey: "AIzaSyB_w9xBOkjJAmp6_sce6drR1Dn3Zco-rrg",
+  authDomain: "shopsharer-675c7.firebaseapp.com",
+  databaseURL: "https://shopsharer-675c7.firebaseio.com",
+  projectId: "shopsharer-675c7",
+  storageBucket: "shopsharer-675c7.appspot.com",
+  messagingSenderId: "606001631410",
+  appId: "1:606001631410:web:0790664d6927e293d239ab",
+  measurementId: "G-6M8X2HGHHX",
+};
+
+const firebaseApp = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+const storage = firebaseApp.storage();
+
+export async function signInWithGoogle() {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  await auth.signInWithPopup(provider);
+  window.location.reload();
+}
+
+export function checkAuth(cb) {
+  auth.onAuthStateChanged(cb);
+}
+
+export async function logOut() {
+  await auth.signOut();
+  window.location.reload();
+}
+
+export async function getCollection(id) {
+  const snapshot = await db.collection(id).get();
+  const data = snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+  console.log(data);
+}
+
+export async function getUserList(userId) {
+  const snapshots = await db
+    .collection("lists")
+    .where("author", "==", userId)
+    .get();
+  return snapshots.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+}
+
+function uploadCoverImage(file) {
+  const uploadTask = storage
+    .ref(`images/${file.name}-${file.lastModified}`)
+    .put(file);
+  return new Promise((resolve, reject) => {
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => console.log("image uploading", snapshot),
+      reject,
+      () => {
+        storage
+          .ref("images")
+          .child(`${file.name}-${file.lastModified}`)
+          .getDownloadURL()
+          .then(resolve);
+      }
+    );
+  });
+}
+
+export async function createList(list, user) {
+  const { name, description, image } = list;
+  await db.collection("lists").add({
+    name,
+    description,
+    image: image ? await uploadCoverImage(image) : null,
+    created: firebase.firestore.FieldValue.serverTimestamp(),
+    author: user.uid,
+    userIds: [user.uid],
+    users: [
+      {
+        id: user.uid,
+        name: user.displayName,
+      },
+    ],
+  });
+}
